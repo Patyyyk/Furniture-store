821368a960804681a5ebb3a2b5d090695eae949c
Piotr <patyyyk@gmail.com>
2017-12-20 10:27:56 +0100

    Creating JSON and basic files for work

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+//Konfiguracja Webpack
+module.exports = {
+  entry: ["whatwg-fetch", "./js/zadanie02.jsx"],
+  output: {
+    filename: "./js/out.js"
+  },
+  devServer: {
+    port: 3006,
+    contentBase: './',
+    inline: true
+  },
+  devtool: "#eval-source-map",
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','stage-2', 'react']
+        }
+      }
+    ]
+  }
+}
