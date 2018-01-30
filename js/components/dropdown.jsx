32c35d712e87693c612d0947cd8d9ec80f2645ff
patyyyk <patyyyk@gmail.com>
2018-01-30 23:57:00 +0100

    Add select instead droopdownmenu...and its work :)

--- js/components/dropdown.jsx
+++ js/components/dropdown.jsx
@@ -3,16 +3,12 @@ import {Link} from 'react-router';
 
 class Dropdown extends React.Component {
   render() {
-    return (<div className="dropdown">
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Categories
-      </button>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <Link className="dropdown-item" to='/products'>Beds</Link>
-        <Link className="dropdown-item" to='/products'>Chairs</Link>
-        <Link className="dropdown-item" to='/products'>Sofas</Link>
-      </div>
-    </div>);
-  } 
+    return (  <select className='custom-select'name="categories">
+    <option value="All products">All products</option>
+    <option value="Beds">Beds</option>
+    <option value="Chairs">Chairs</option>
+    <option value="Sofas">Sofas</option>
+  </select>);
+  }
 }
 export default Dropdown;
