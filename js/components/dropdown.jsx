5a693582dd193094524211d866623e13633561e5
patyyyk <patyyyk@gmail.com>
2018-01-31 21:03:15 +0100

    Dropdown not working-event problem

--- js/components/dropdown.jsx
+++ js/components/dropdown.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownValue:''
+    }
+  }
+
+dropdownChange = (event) => {
+  this.setState =({
+    dropdownValue: event.target.value
+  })
+  console.log(this.state.dropdownValue);
+}
+
   render() {
-    return (  <select className='custom-select'name="categories">
-    <option value="All products">All products</option>
-    <option value="Beds">Beds</option>
-    <option value="Chairs">Chairs</option>
-    <option value="Sofas">Sofas</option>
-  </select>);
+
+    return (
+              <select value={this.state.dropdownValue} onChange={this.dropdownChange} className='custom-select'>
+                <option value="All products">All products</option>
+                <option value="Beds">Beds</option>
+                <option value="Chairs">Chairs</option>
+                <option value="Sofas">Sofas</option>
+              </select>
+          );
   }
 }
 export default Dropdown;
