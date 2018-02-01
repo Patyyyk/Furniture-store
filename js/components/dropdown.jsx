5a5556fcd6b8b5c1f9ddda98379d9e99ab896fa0
patyyyk <patyyyk@gmail.com>
2018-02-01 11:31:42 +0100

    Dropdown is working without filtering content

--- js/components/dropdown.jsx
+++ js/components/dropdown.jsx
@@ -5,27 +5,21 @@ class Dropdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dropdownValue:''
+      dropdownValue: ''
     }
+  };
+
+  dropdownChange = (event) => {
+    this.setState({dropdownValue: event.target.value})
   }
 
-dropdownChange = (event) => {
-  this.setState =({
-    dropdownValue: event.target.value
-  })
-  console.log(this.state.dropdownValue);
-}
-
   render() {
-
-    return (
-              <select value={this.state.dropdownValue} onChange={this.dropdownChange} className='custom-select'>
-                <option value="All products">All products</option>
-                <option value="Beds">Beds</option>
-                <option value="Chairs">Chairs</option>
-                <option value="Sofas">Sofas</option>
-              </select>
-          );
+    return (<select value={this.state.dropdownValue} onChange={this.dropdownChange} className='custom-select'>
+      <option value="All products">All products</option>
+      <option value="Beds">Beds</option>
+      <option value="Chairs">Chairs</option>
+      <option value="Sofas">Sofas</option>
+    </select>);
   }
 }
 export default Dropdown;
